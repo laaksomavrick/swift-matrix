2a895293affbdcf80fb1605aaaab09b21cafea00
mlaakso@dev <laakso.mavrick@gmail.com>
2017-11-04 12:04:04 -0400

    Initial Commit

---
+++ SwiftMatrix/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftMatrix
+//
+//  Created by Mavrick Laakso on 2017-11-04.
+//  Copyright © 2017 Mavrick Laakso. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
