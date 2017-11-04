791c35b501377975863c72698a474abd42cf7938
mlaakso@dev <laakso.mavrick@gmail.com>
2017-11-04 12:40:33 -0400

    Initial commit

--- SwiftMatrix/main.swift
+++ SwiftMatrix/main.swift
@@ -7,6 +7,34 @@
 //
 
 import Foundation
+import Darwin.ncurses
 
-print("Hello, World!")
+initscr()                   // Init window. Must be first
+cbreak()
+noecho()                    // Don't echo user input
+nonl()                      // Disable newline mode
+intrflush(stdscr, true)     // Prevent flush
+keypad(stdscr, true)        // Enable function and arrow keys
+curs_set(1)                 // Set cursor to invisible
 
+addch(0)
+move(5, 0)
+addch(1)
+move(5, 5)
+addch(2)
+refresh()
+
+
+// Wait for user input
+// Exit on 'q'
+while true {
+    
+    switch getch() {
+        
+    case Int32(UnicodeScalar("q").value):
+        endwin()
+        exit(EX_OK)
+    default: true
+    }
+    
+}
